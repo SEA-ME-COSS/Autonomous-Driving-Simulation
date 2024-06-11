5aa8e058ad95809745ab998d7843801da98f82c0
dongdongO <tjehdghks4356@gmail.com>
2024-06-11 16:15:58 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -3,19 +3,14 @@
 CURRENT_DIR=$(pwd)
 
 SOURCE_DIR="$CURRENT_DIR/simulation_ws/src/sim/models"
-
 GAZEBO_MODELS_DIR="$HOME/.gazebo/models"
+PACKAGE_XML_PATH="$CURRENT_DIR/simulation_ws/src/sim/package.xml"
+TARGET_DIR="$CURRENT_DIR/simulation_ws/install/sim/share/"
 
 cp -r "$SOURCE_DIR"/* "$GAZEBO_MODELS_DIR" || { echo "Error: Failed to copy models to $GAZEBO_MODELS_DIR"; exit 1; }
 
 echo "Models have been successfully copied to $GAZEBO_MODELS_DIR"
 
-PACKAGE_XML_PATH="$CURRENT_DIR/simulation_ws/src/sim/package.xml"
-
-sed -i "s|<gazebo_ros gazebo_model_path = \".*\"/>|<gazebo_ros gazebo_model_path = \"$HOME/.gazebo/models\"/>|g" "$PACKAGE_XML_PATH"
+sed -i "s|\(gazebo_model_path *= *\"\)[^\"]*\"|\1$TARGET_DIR\"|g" "$PACKAGE_XML_PATH"
 
 echo "package.xml has been updated with the correct gazebo_model_path"
-
-cd "$CURRENT_DIR/simulation_ws"
-
-echo "Current directory is now $(pwd)/simulation_ws"
